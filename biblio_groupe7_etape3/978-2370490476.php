e0188f43f7684fd7758477d7cd3c89ddaaa05310
matteo <mrcraft758@hotmail.fr>
2020-12-16 00:10:38 +0100

    new

---
+++ biblio_groupe7_etape3/978-2370490476.php
@@ -0,0 +1,24 @@
+<?php
+$page_title = "Library 404";
+require "header.php";
+?>
+<link rel="stylesheet" href="\biblio_groupe7_etape3\css\style_isbn">
+  <body>
+  <div class="img">
+    <div class="position">
+    <img class="img1" src="image/couverture/Le-Cinquieme-Principe.jpg" title="Le Cinquième Principe">
+    </div>
+    <div class="texte">
+      <p><em>Le Cinquième Principe</em>,de Vittorio Catani</p>
+      <p>Isbn : 978-2370490476</p>
+      <p>Editeur : La Volte</p>
+      <p>date de sortie : 21 septembre 2017</p>
+      <p>Genre : Roman de science fiction</p>
+      <p>Nombre de pages : 564 pages</p>
+    </div>
+  </div>
+  </body>
+  <?php
+  require "footer.php";
+  ?>
+</html>
---
+++ biblio_groupe7_etape3/978-2843449246.php
@@ -0,0 +1,24 @@
+<?php
+$page_title = "Library 404";
+require "header.php";
+?>
+<link rel="stylesheet" href="\biblio_groupe7_etape3\css\style_isbn">
+  <body>
+  <div class="img">
+    <div class="position">
+    <img class="img1" src="image/couverture/24-Vues-du-Mont-Fuji.jpg" title="24 Vues du Mont Fuji">
+    </div>
+    <div class="texte">
+      <p><em>Le Crépuscule des Dieux</em>, de Stéphane Przybylski </p>
+      <p>Isbn : 978-2843449246</p>
+      <p>Editeur : Pocket</p>
+      <p>Date de sortie : 10 janvier 2019</p>
+      <p>Genre : Roman de science fiction</p>
+      <p>Nombre de pages : 136 pages</p>
+    </div>
+  </div>
+  </body>
+  <?php
+  require "footer.php";
+  ?>
+</html>
