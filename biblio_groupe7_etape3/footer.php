10d52411f48f25e834e242fa7e62b64bf4512710
matteo <>
2021-01-03 17:38:43 +0100

    fin etape 3

--- biblio_groupe7_etape3/footer.php
+++ biblio_groupe7_etape3/footer.php
@@ -1,8 +1,10 @@
 <footer>
+  <div class="foot">
   <link rel="stylesheet" href="\biblio_groupe7_etape3\css\footer.css">
   <ul>
     <li><a href="contact.php">Contact:</a></li>
     <li>m.beaulieu@ecole-ipssi.net</li>
     <li>m.chen@ecole-ipssi.net</li>
     </ul>
+  </div>
 </footer>
