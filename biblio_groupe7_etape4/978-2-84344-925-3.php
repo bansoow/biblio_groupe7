78904ef24fa2f78d0238a1785e845a632317fca5
matteo <>
2021-01-03 17:45:30 +0100

    etape4

---
+++ biblio_groupe7_etape4/978-2-84344-925-3.php
@@ -0,0 +1,24 @@
+<?php
+$page_title = "Library 404";
+require "header.php";
+?>
+<link rel="stylesheet" href="\biblio_groupe7_etape3\css\style_isbn">
+  <body>
+  <div class="img">
+    <div class="position">
+    <img class="img1" src="image/couverture/Le-Sultan-des-Nuages.jpg" title="Le Sultan des Nuages">
+    </div>
+    <div class="texte">
+      <p><em>Le Sultan des nuages</em>,de Geoffrey A. Landis</p>
+      <p>Isbn : 978-2-84344-925-3, </p>
+      <p>Editeur : Le Belial</p>
+      <p>date de sortie : 31 août 2017</p>
+      <p>Genre : Roman de science fiction</p>
+      <p>Nombre de pages : 120 pages</p>
+    </div>
+  </div>
+  </body>
+  <?php
+  require "footer.php";
+  ?>
+</html>
---
+++ biblio_groupe7_etape4/978-2-84344-926-0.php
@@ -0,0 +1,24 @@
+<?php
+$page_title = "Library 404";
+require "header.php";
+?>
+<link rel="stylesheet" href="\biblio_groupe7_etape3\css\style_isbn">
+  <body>
+  <div class="img">
+    <div class="position">
+    <img class="img1" src="image/couverture/Le-Crepuscule-des-Dieux.jpg" title="Le Crépuscule des dieux">
+    </div>
+    <div class="texte">
+      <p><em>Le Crépuscule des dieux</em>,de Stéphane Przybyski</p>
+      <p>Isbn : 978-2-84344-926-0</p>
+      <p>Editeur : Pocket</p>
+      <p>date de sortie : 10 Janvier 2019</p>
+      <p>Genre : Roman de science fiction</p>
+      <p>Nombre de pages : 480 pages</p>
+    </div>
+  </div>
+  </body>
+  <?php
+  require "footer.php";
+  ?>
+</html>
