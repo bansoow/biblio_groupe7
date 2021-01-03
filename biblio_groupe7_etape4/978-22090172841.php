311e086a0e6ebc62c3e67eb0d0a7c6d4face00a3
matteo <>
2021-01-03 17:51:50 +0100

    etape4

---
+++ biblio_groupe7_etape4/978-22090172841.php
@@ -0,0 +1,24 @@
+<?php
+$page_title = "Library 404";
+require "header.php";
+?>
+<link rel="stylesheet" href="\biblio_groupe7_etape3\css\style_isbn">
+  <body>
+  <div class="img">
+    <div class="position">
+    <img class="img1" src="image/couverture/cinquieme-saison.jpg" title="La Cinquieme saison">
+    </div>
+    <div class="texte">
+      <p><em>La Cinquième Saison</em>,de NK Jemisin </p>
+      <p>Isbn : 978-22090172841</p>
+      <p>Editeur : j’ai lu</p>
+      <p>date de sortie : 28 aouts 2019</p>
+      <p>Genre : Roman de science fiction</p>
+      <p>Nombre de pages : 480 pages</p>
+    </div>
+  </div>
+  </body>
+  <?php
+  require "footer.php";
+  ?>
+</html>
---
+++ biblio_groupe7_etape4/footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <link rel="stylesheet" href="\biblio_groupe7_etape3\css\footer.css">
+  <ul>
+    <li><a href="contact.php">Contact:</a></li>
+    <li>m.beaulieu@ecole-ipssi.net</li>
+    <li>m.chen@ecole-ipssi.net</li>
+    </ul>
+</footer>
