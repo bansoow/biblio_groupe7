4a8246d3204b086f2b76945ea88e3cea3bdc8563
matteo <>
2021-01-14 11:45:27 +0100

    ajout jointure

--- biblio_groupe7_etape4/livre_config.php
+++ biblio_groupe7_etape4/livre_config.php
@@ -1,6 +1,10 @@
 <?php
 require 'config.php';
 if (isset($_GET['isbn'])) {
-    $biblio = $bdd->query("SELECT * FROM livre WHERE isbn =".$_GET['isbn']);
+    $biblio = $bdd->query("SELECT *, editeur.libelle AS editeurs, genre.libelle AS genres FROM livre JOIN auteur ON auteur.idPersonne=livre.auteur
+                                               JOIN personne ON personne.id=auteur.idPersonne
+                                               JOIN editeur ON editeur.id=livre.editeur
+                                               JOIN genre ON genre.id=livre.genre
+                                               WHERE isbn =".$_GET['isbn'] );
     $biblio->setFetchMode(PDO::FETCH_ASSOC);
 }
